aef50f86a5f4202a4749a91ffdf445aa4a402371
Eddy Caldas <eddycaldas@live.com>
2018-10-23 20:44:49 -0600

    slice and splice

--- src/App.js
+++ src/App.js
@@ -14,14 +14,11 @@ class App extends Component {
       showPersons: false
     }
 
-    switchNameHandler = (newName) => {
-      this.setState( {
-        persons: [
-          { name: newName, age: 28 },
-          { name: "Manu", age: 29 },
-          { name: "Erica", age: 31 }
-        ]
-      } )
+    deletePersonHandler = (index) => {
+      const persons = [ ...this.state.persons ]
+     // const persons = this.state.persons.slice();    
+      persons.splice( index, 1 )           
+      this.setState ({ persons: persons })
     }
 
     namechangedHandler = (event) => {
@@ -54,8 +51,9 @@ class App extends Component {
     if ( this.state.showPersons ) {
       persons = (
           <div>
-            { this.state.persons.map(person => {
+            { this.state.persons.map( ( person, index ) => {
               return <Person 
+                click={ () => this.deletePersonHandler( index ) }
                 name={ person.name } 
                 age={ person.age }/>
             }) }
